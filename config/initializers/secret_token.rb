1e83c46ab391e08e58f207f67bcffff95acbe795
Azarel Doroteo Pacheco <azareldp@gmail.com>
2013-05-05 11:35:29 -0500

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sevakindle::Application.config.secret_key_base = '7e44a880dff7ec6baa11e87471f4d41f41fc6de297d0a18940710d9bc7f936d970b3342e9f358201611f302e00320deffb89e10e6b7c126e431a98593286e2b8'
