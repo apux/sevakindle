1fa5d54d8212b4cdf641c912c585bc9fc26c0f34
Azarel Doroteo Pacheco <azareldp@gmail.com>
2013-08-11 12:33:12 -0500

    Add root route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Sevakindle::Application.routes.draw do
-  get "cuentos/index"
   resources :publicaciones
 
   resources :tipos_publicaciones
@@ -12,7 +11,7 @@ Sevakindle::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'cuentos#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
