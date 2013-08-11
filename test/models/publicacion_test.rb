0f13a4652d94cad7d16b53cff7c80287362f726b
Azarel Doroteo Pacheco <azareldp@gmail.com>
2013-08-11 10:57:06 -0500

    Refactor to clear the code.

--- test/models/publicacion_test.rb
+++ test/models/publicacion_test.rb
@@ -6,26 +6,20 @@ describe Publicacion do
   end
 
   it "must be valid" do
-    @publicacion.valid?.must_equal true
+    assert @publicacion.valid?
   end
 
   describe 'scopes' do
     describe '.cuentos' do
       it "returns a publicacion with tipo_publicacion 'Cuento'" do
-        tipo_publicacion = TipoPublicacion.new nombre: 'Cuento'
-        tipo_publicacion.save!
-        publicacion = Publicacion.new titulo: 'aaa', texto: 'bbb', tipo: tipo_publicacion
-        publicacion.save!
-
+        tipo_publicacion = TipoPublicacion.create nombre: 'Cuento'
+        publicacion = Publicacion.create titulo: 'aaa', texto: 'bbb', tipo: tipo_publicacion
         Publicacion.cuentos.count.must_equal 1
       end
 
       it "does not return any publicacion with tipo_publicacion different than 'Cuento'" do
-        tipo_publicacion = TipoPublicacion.new nombre: 'Ensayo'
-        tipo_publicacion.save!
-        publicacion = Publicacion.new titulo: 'aaa', texto: 'bbb', tipo: tipo_publicacion
-        publicacion.save!
-
+        tipo_publicacion = TipoPublicacion.create nombre: 'Ensayo'
+        publicacion = Publicacion.create titulo: 'aaa', texto: 'bbb', tipo: tipo_publicacion
         Publicacion.cuentos.count.must_equal 0
       end
     end
